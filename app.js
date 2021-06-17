b7e9dd850d904a9d2f07391112f235b9dda21e0b
Odaialqaiem <85878279+OdaiAlqaiem@users.noreply.github.com>
2021-06-18 01:23:08 +0300

    Update app.js
    add

--- app.js
+++ app.js
@@ -8,6 +8,32 @@ var age = prompt("What is your age ")
 age = 2021 - age
 alert("you were born in this year " + age)
 
+<<<<<<< HEAD
+if (age >= 20) {
+
+  var favplayer = prompt("What is favourite player (Zedan , Maradona) ? ")
+
+  while (favplayer != "Zedan" && favplayer != "Maradona"){
+    favplayer = prompt("you intered wrong chooses please try again")
+  }
+
+
+  var counter = prompt("How many items do you want ? " + favplayer)
+  console.log("the number of items =" + counter);
+
+
+  if (favplayer == "Zedan") {
+    document.write("<p>" + "great player in the history Zedan we have it here so you van watch it" + "</p>")
+  }
+
+  else if (favplayer == "Maradona") {
+    document.write("<p>" + "great player in the history Maradona we have it here so you van watch it" + "</p>")
+  }
+
+  else {
+    alert("Sorry, that is not found")
+  }
+=======
 var favplayer = prompt("What is favourite player (Zedan , Maradona) ? ")
 
 if (favplayer == "Zedan") {
@@ -20,7 +46,15 @@ else if (favplayer == "Maradona") {
 
 else {
   alert("Sorry, that is not found")
+>>>>>>> 07265f522e867117f53d707f6ded9991c69cec48
 }
+
+
+
+
+
+
+
 
 
 
