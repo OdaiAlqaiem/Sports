8481843936a547f74b848cb187c048e3a266a0fc
OdaiAlqaiem <>
2021-06-17 22:09:50 +0000

    hi

---
+++ app.js
@@ -0,0 +1,42 @@
+alert("Welcome to Sports website")
+
+var name = prompt("What is your name")
+alert("Happy to meet you " + name)
+
+alert("This site calculates your age")
+var age = prompt("What is your age ")
+age = 2021 - age
+alert("you were born in this year " + age)
+
+if (age >= 20) {
+
+  var favplayer = prompt("What is favourite player (Zedan , Maradona) ? ")
+
+  while (favplayer != "Zedan" && favplayer != "Maradona"){
+    favplayer = prompt("you intered wrong chooses please try again")
+  }
+
+
+  var counter = prompt("How many items do you want ? " + favplayer)
+  console.log("the number of items =" + counter);
+
+
+  if (favplayer == "Zedan") {
+    document.write("<p>" + "great player in the history Zedan we have it here so you van watch it" + "</p>")
+  }
+
+  else if (favplayer == "Maradona") {
+    document.write("<p>" + "great player in the history Maradona we have it here so you van watch it" + "</p>")
+  }
+
+  else {
+    alert("Sorry, that is not found")
+  }
+}
+
+
+
+
+
+
+
